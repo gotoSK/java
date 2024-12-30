5a0e973df6c90f28573d029c2564a9d37e6fc31e
unknown <karkisugam34@gmail.com>
2024-12-30 14:18:36 +0545

    runner

--- swingapps/src/swingapps/SwingDemo.java
+++ swingapps/src/swingapps/SwingDemo.java
@@ -3,9 +3,11 @@ package swingapps;
 public class SwingDemo {
     public static void main(String[] args) {
         // GUIControls.guiControls();
-        // Menu1.menu1();
+        // Menu1.menu1(false);
         // Menu2.menu2();
         // Menu3.menu3();
-        Menu4.menu4();
+        // Menu4.menu4();
+
+        new Sum();
     }
 }
